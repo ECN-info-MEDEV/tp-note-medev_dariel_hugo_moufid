282d78739ee5a572887e917394c2bd2a551b53ba
hugorufato <hugoc.rufato@usp.br>
2024-01-17 21:26:20 +0100

    'test'

--- medev-ds/src/main/java/org/centrale/app/App.java
+++ medev-ds/src/main/java/org/centrale/app/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Teste Connection - Dariel");
-        System.out.println("Teste Connection - Moufid");
+        System.out.println("Teste Connection - Moufid
     }
 }
