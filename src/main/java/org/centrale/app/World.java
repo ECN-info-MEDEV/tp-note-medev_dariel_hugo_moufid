826f80e6cb2edfc1294ce4424bfb164ad59ecca4
Dariel Bezerra <darielbezerra13@gmail.com>
2024-01-18 11:58:46 +0100

    add UT World class

--- src/main/java/org/centrale/app/World.java
+++ src/main/java/org/centrale/app/World.java
@@ -3,29 +3,32 @@ package org.centrale.app;
 import java.util.Scanner;
 
 public class World {
+
     private Joueur joueur1;
     private Joueur joueur2;
 
-    public World(){
-        this.joueur1 = new Joueur("Joueur 1","joueur1@gmail.com");
-        this.joueur2 = new Joueur("Joueur 2","joueur2@gmail.com");
+    public World() {
+        this.joueur1 = new Joueur("Joueur 1", "joueur1@gmail.com");
+        this.joueur2 = new Joueur("Joueur 2", "joueur2@gmail.com");
     }
 
-    /**Methode tourDeJeu qui declanche un tour pour l'un des deux joueurs
-     * qui choisit une position cible pour attaquer
+    /**
+     * Methode tourDeJeu qui declanche un tour pour l'un des deux joueurs qui
+     * choisit une position cible pour attaquer
+     *
      * @param joueur1
      * @param joueur2
      */
-    public void tourDeJeu(Joueur joueur1, Joueur joueur2){
+    public void tourDeJeu(Joueur joueur1, Joueur joueur2) {
         Scanner input = new Scanner(System.in);
-        System.out.println("C'est le tour du "+joueur1.getNom());
-        System.out.println("Affichage de la grille du "+joueur1.getNom());
+        System.out.println("C'est le tour du " + joueur1.getNom());
+        System.out.println("Affichage de la grille du " + joueur1.getNom());
         joueur1.getGrille().affichage();
-        System.out.println("Affichage de la grille du "+joueur2.getNom());
+        System.out.println("Affichage de la grille du " + joueur2.getNom());
         joueur2.getGrille().affichageCache();
-        System.out.println("A toi d'attaquer "+joueur1.getNom()+" !");
-        int x,y;
-        do{
+        System.out.println("A toi d'attaquer " + joueur1.getNom() + " !");
+        int x, y;
+        do {
             System.out.println("Vous devez choisir un point d'attaque x,y");
             System.out.println("Attention à bien choisir des valeurs entre 0 et 4 !");
             System.out.println("Choisir x");
@@ -34,7 +37,7 @@ public class World {
             System.out.println("Choisir y");
             String yString = input.nextLine();
             y = Integer.parseInt(yString);
-        }while(x<0 || x>4 || y<0 || y>4);
-        joueur1.attaque(joueur2,x,y);
+        } while (x < 0 || x > 4 || y < 0 || y > 4);
+        joueur1.attaque(joueur2, x, y);
     }
 }
---
+++ src/test/java/org/centrale/app/WorldTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package org.centrale.app;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author darielbezerra
+ */
+/**
+ * Classe de test pour la classe World.
+ */
+public class WorldTest {
+   /**
+     * Teste la méthode tourDeJeu en simulant l'entrée utilisateur.
+     */
+    @Test
+    public void testTourDeJeu() {
+        // Créer une instance de World
+        World world = new World();
+
+        // Configurer les objets joueur pour le test
+        Joueur joueur1 = new Joueur("Joueur 1", "joueur1@gmail.com");
+        Joueur joueur2 = new Joueur("Joueur 2", "joueur2@gmail.com");
+        joueur1.setGrille(new Grille());
+        joueur2.setGrille(new Grille());
+
+        // Simuler l'entrée de l'utilisateur
+        String input = "0\n0\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // Appeler la méthode à tester
+        world.tourDeJeu(joueur1, joueur2);
+
+
+        // Vérifier si le message du tour est correct
+        assertEquals("Le message du tour doit être correct", "C'est le tour du " + joueur1.getNom(), "C'est le tour du " + joueur1.getNom());
+
+
+        // Réinitialiser l'entrée standard
+        System.setIn(System.in);
+    }
+}
